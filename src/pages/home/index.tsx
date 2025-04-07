d103bcaef0b496c7c3b848a561969f89cf5c52f4
Gstv <gstv.almeida09@gmail.com>
2025-04-07 13:11:16 -0300

    feat: adicinando requesição na página home

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,13 +1,67 @@
+import { useState, useEffect } from "react";
+
+import { api } from "../../services/api";
+
 import styles from "./style.module.css";
 
+interface Transaction {
+  id: number;
+  transactionType: string;
+  type: string;
+  price: number;
+  createdAt: Date;
+}
+
+interface User {
+  id: number;
+  name: string;
+  balance: number;
+}
+
+interface UserRequest {
+  transactions: [
+    {
+      id: number;
+      transaction_type: string;
+      type: string;
+      price: number;
+      created_at: Date;
+    }
+  ];
+}
+
 function Home() {
+  const [transactions, setTransactions] = useState<Transaction[]>([]);
+  const [user, setUser] = useState<User>({
+    id: 1,
+    name: "Gustavo André",
+    balance: 5000,
+  });
+
+  useEffect(() => {
+    api
+      .get<UserRequest>(`users/${user.id}`)
+      .then(({ data: { transactions } }) => {
+        setTransactions(
+          transactions.map((transaction) => ({
+            ...transaction,
+            transactionType: transaction.transaction_type,
+            createdAt: transaction.created_at,
+          }))
+        );
+      })
+      .catch((error) => {
+        console.error("Erro ao buscar dados:", error);
+      });
+  }, [user]);
+
   return (
     <div className={styles.container}>
       <header className={styles.header}>MAGNUM BANK</header>
 
       <section className={styles.balanceCard}>
         <h2>Saldo disponível</h2>
-        <p className={styles.balance}>R$ 5.000,00</p>
+        <p className={styles.balance}>R$ {user.balance}</p>
       </section>
 
       <section className={styles.actions}>
@@ -18,27 +72,20 @@ function Home() {
       <section className={styles.transactions}>
         <h3>Últimas transações</h3>
         <ul>
-          <li>
-            <div className={styles.transactionDetail}>
-              <span>Pagamento - Conta de luz</span>
-              <span className={styles.negative}>-R$ 200,00</span>
-            </div>
-            <div className={styles.transactionDate}>05/04/2025</div>
-          </li>
-          <li>
-            <div className={styles.transactionDetail}>
-              <span>PIX recebido - Ana</span>
-              <span className={styles.positive}>+R$ 300,00</span>
-            </div>
-            <div className={styles.transactionDate}>04/04/2025</div>
-          </li>
-          <li>
-            <div className={styles.transactionDetail}>
-              <span>Saque 24h</span>
-              <span className={styles.negative}>-R$ 500,00</span>
-            </div>
-            <div className={styles.transactionDate}>03/04/2025</div>
-          </li>
+          {transactions.map((transaction) => (
+            <li key={transaction.id}>
+              <div className={styles.transactionDetail}>
+                <span>{transaction.transactionType}</span>
+                <span className={styles.negative}>
+                  {transaction.type === "income" ? "+" : "-"}R${" "}
+                  {transaction.price}
+                </span>
+              </div>
+              <div className={styles.transactionDate}>
+                {transaction.createdAt}
+              </div>
+            </li>
+          ))}
         </ul>
       </section>
     </div>
