b74f95f278bf391d4f916dc0dceb08bd5a9fdec5
Gstv <gstv.almeida09@gmail.com>
2025-04-08 15:42:51 -0300

    feat: atualizando saldo e edição na requisiçao de transação

--- src/pages/transaction/index.tsx
+++ src/pages/transaction/index.tsx
@@ -5,6 +5,15 @@ import { IMaskInput } from "react-imask";
 import { api } from "../../services/api";
 
 import styles from "./style.module.css";
+import { useEffect, useState } from "react";
+import { priceFormatter } from "../../utils/formatter";
+
+interface User {
+  id: number;
+  name: string;
+  balance: number;
+  transactionPassword: number;
+}
 
 interface BaseProps {
   created_at: Date;
@@ -41,30 +50,85 @@ function Transaction() {
   } = useForm<DataProps>({
     defaultValues: {
       transaction_type: "TED",
+      price: 0,
     },
   });
+  const [user, setUser] = useState<User>();
   const isPixType = watch("transaction_type");
+  const priceWatch = watch("price");
+  const isSufficientBalance = user?.balance - priceWatch >= 0;
+  console.log(isSufficientBalance);
   const navigate = useNavigate();
 
-  function handleCreateTransactions(data: DataProps) {
+  async function handleCreateTransactions(data: DataProps) {
     const newTransaction = {
       ...data,
-      type: "income",
-      user_id: 1,
     };
 
-    api
-      .post(`transactions`, newTransaction)
-      .then(({ data }) => {
-        console.log(data);
-      })
-      .catch((error) => {
-        console.error("Erro ao buscar dados:", error);
+    try {
+      const responseUsers = await api.get("users");
+
+      const userSender = responseUsers.data.find(
+        (userSenderData) => userSenderData?.id === user.id
+      );
+
+      const userReceiver = responseUsers.data.find(
+        (userReceiverData) =>
+          userReceiverData?.document === newTransaction.document
+      );
+
+      // adicionando transaction relacionado ao remetente e depois subtraindo saldo
+      await api.post("transactions", {
+        ...newTransaction,
+        user_id: user?.id,
+        type: "outcome",
       });
+
+      await api.patch(`users/${userSender.id}`, {
+        balance: userSender?.balance - newTransaction.price,
+      });
+
+      // adicionando transaction relacionado ao destinatário (caso exista) e depois somando saldo
+      if (userReceiver) {
+        await api.post("transactions", {
+          ...newTransaction,
+          user_id: userReceiver.id,
+          type: "income",
+        });
+
+        await api.patch(`users/${userReceiver.id}`, {
+          balance: userReceiver?.balance + newTransaction.price,
+        });
+      }
+    } catch (error) {
+      console.error("Erro ao fazer transação:", error);
+    }
 
     reset();
     navigate("/");
   }
+
+  useEffect(() => {
+    const storedUser = localStorage.getItem("user");
+
+    if (storedUser) {
+      const { id } = JSON.parse(storedUser);
+
+      api
+        .get(`users/${id}`)
+        .then(({ data }) => {
+          setUser({
+            id: data.id,
+            name: data.name,
+            balance: data.balance,
+            transactionPassword: data.transaction_password,
+          });
+        })
+        .catch((error) => {
+          console.error("Erro ao buscar dados:", error);
+        });
+    }
+  }, []);
 
   return (
     <form
@@ -74,9 +138,11 @@ function Transaction() {
       <h1>MAGNUM BANK</h1>
       <h2>TRANSFERÊNCIA</h2>
 
-      <div className={styles.saldo}>Saldo atual: R$ 5.000,00</div>
+      <div className={styles.saldo}>
+        Saldo atual: {priceFormatter.format(user?.balance)}
+      </div>
 
-      <label>Selecione o tipo de transação</label>
+      <label htmlFor="transaction_type">Selecione o tipo de transação</label>
       <div className={styles.radioGroup}>
         <label>
           <input type="radio" {...register("transaction_type")} value="TED" />{" "}
@@ -88,17 +154,10 @@ function Transaction() {
         </label>
       </div>
 
-      <label>CPF/CNPJ do favorecido</label>
-      <IMaskInput
-        mask="000.000.000-00"
-        placeholder="000.000.000-00"
-        onAccept={(value) => console.log(value)}
-        required
-        overwrite
-        {...register("document")}
-      />
+      <label htmlFor="document">CPF/CNPJ do favorecido</label>
+      <input placeholder="000.000.000-00" required {...register("document")} />
 
-      <label>Nome do favorecido</label>
+      <label htmlFor="name">Nome do favorecido</label>
       <input
         type="text"
         placeholder="João Silva"
@@ -108,7 +167,7 @@ function Transaction() {
 
       {isPixType === "PIX" ? (
         <>
-          <label>Chave PIX</label>
+          <label htmlFor="pix_key">Chave PIX</label>
           <input
             type="text"
             placeholder="joao@email.com"
@@ -118,7 +177,7 @@ function Transaction() {
         </>
       ) : (
         <>
-          <label>Banco</label>
+          <label htmlFor="bank">Banco</label>
           <input
             type="text"
             placeholder="Banco"
@@ -126,45 +185,35 @@ function Transaction() {
             {...register("bank")}
           />
 
-          <label>Agência</label>
-          <IMaskInput
-            mask="0000"
-            placeholder="4444"
-            required
-            {...register("agency")}
-          />
+          <label htmlFor="agency">Agência</label>
+          <input placeholder="4444" required {...register("agency")} />
 
-          <label>Conta</label>
-          <IMaskInput
-            mask="00000000-0"
-            placeholder="03763500-3"
-            required
-            {...register("account")}
-          />
+          <label htmlFor="account">Conta</label>
+          <input placeholder="03763500-3" required {...register("account")} />
         </>
       )}
 
-      <label>Valor a transferir (R$)</label>
-      <IMaskInput
-        mask={Number}
-        radix=","
-        thousandsSeparator="."
-        scale={2}
-        signed={false}
-        padFractionalZeros={true}
-        normalizeZeros={true}
+      <label htmlFor="price">Valor a transferir (R$)</label>
+      <input
         placeholder="300,00"
         required
         {...register("price", { valueAsNumber: true })}
       />
+      {!isSufficientBalance && (
+        <p className={styles.alertBalance}>Saldo insuficiente!</p>
+      )}
 
-      <label>Data da transferência</label>
+      <label htmlFor="date">Data da transferência</label>
       <input type="date" {...register("created_at")} />
 
-      <label>Senha de transação</label>
+      <label htmlFor="password">Senha de transação</label>
       <input type="password" required {...register("password")} />
 
-      <button type="submit" disabled={isSubmitting}>
+      <button
+        className={styles.buttonSubmit}
+        type="submit"
+        disabled={isSubmitting || !isSufficientBalance}
+      >
         Confirmar Transferência
       </button>
     </form>
